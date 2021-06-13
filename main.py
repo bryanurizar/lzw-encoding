6dbe69ccc8f09ef876c90ff7dc76a761a83041e5
Bryan Urizar <bryan.urizar@outlook.com>
2021-06-13 01:52:01 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+encoding_dictionary = {'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 'f': '6', 'g': '7', 'h': '8', 'i': '9', 'j': '10', 'k': '11', 'l': '12',
+                       'm': '13', 'n': '14', 'o': '15', 'p': '16', 'q': '17', 'r': '18', 's': '19', 't': '20', 'u': '21', 'v': '22', 'w': '23', 'x': '24', 'y': '25', 'z': '26', ' ': '27'}
+
+text = 'a sample sentence that compares basic encoding against lzw encoding and outputs the character count for both basic encoding and lzw encoding'
+
+text_list = text.split(' ')
+encoding_dictionary_keys = encoding_dictionary.keys()
+
+
+def basic_encoding(text):
+    basic_encoding = ''
+    for character in text:
+        if character in encoding_dictionary_keys:
+            character = encoding_dictionary[character]
+            basic_encoding += character
+    print('Basic encoding uses: ' + str(len(basic_encoding)) + ' characters')
+    print('Basic Encoding:' + basic_encoding)
+    return ''
+
+
+def LZW_encoding(text):
+    encoded_text = []
+    last_value_used = int(list(encoding_dictionary.values())[-1])
+
+    for word in text_list:
+        if word in encoding_dictionary_keys:
+            encoded_text.append(encoding_dictionary[word])
+        else:
+            last_value_used += 1
+            encoding_dictionary[word] = str(last_value_used)
+            encoded_word = ''
+            for character in word:
+                character = encoding_dictionary[character]
+                encoded_word += character
+            encoded_text.append(encoded_word)
+
+    LZW_encoded_text = encoding_dictionary[' '].join(encoded_text)
+    print('LZW encoding uses: ' + str(len(LZW_encoded_text)) + ' characters')
+    print('LZW Encoded Text:' + LZW_encoded_text)
+    return ''
+
+
+print(basic_encoding(text))
+print(LZW_encoding(text))
